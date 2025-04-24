46102a6902770ea7a28713051696be037710cff6
atfaive <danilkakuliew@gmail.com>
2025-04-24 19:56:20 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using GachiHubBackend.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddSignalR(options =>
+{
+    options.MaximumReceiveMessageSize = 102400; // 100kb
+    options.StreamBufferCapacity = 20;
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapHub<AudioHub>("/audio");
+
+app.Run();
